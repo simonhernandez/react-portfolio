c78e293bc20cb46467efa00c89066ee250e33630
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-09-09 17:37:00 -0400

    add: tab funcionality

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import { Wrapper } from "../components";
 import { layout, typography, spacing } from "../styles/style";
-import { skills } from "../constants";
+import { skills, about } from "../constants";
 import { simonHero } from "../assets";
 
 const About = () => {
@@ -25,7 +25,7 @@ const About = () => {
           <div className="flex-1">
             <div class="border-b border-b-slate-400 dark:border-gray-700">
               <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-                <li class="mr-2">
+                {/* <li class="mr-2">
                   <button
                     class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group active"
                   >
@@ -61,7 +61,30 @@ const About = () => {
                     </svg>
                     Work
                   </button>
-                </li>
+                </li> */}
+                {about.map((item, index) => {
+                  return <li key={item.id} className={`${about.length !== index - 1 ? 'mr-2' : ''}`}>
+                    <button
+                    class={`inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 ${index === currentIndex ? 'border-b-primary text-primary bg-primary_very_light' : ''}`}
+                    onClick={() => setCurrentIndex(index)}
+                  >
+                    <svg
+                      aria-hidden="true"
+                      class="mr-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    {item.title}
+                  </button>
+                  </li>
+                })}
               </ul>
             </div>
             <div>{currentTab && <div>{currentTab.title}</div>}</div>
