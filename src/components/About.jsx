88463580fd0bcfc5853e5067b190cf18ac410e3d
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-09-09 17:15:25 -0400

    feat: about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,131 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Wrapper } from "../components";
+import { layout, typography, spacing } from "../styles/style";
+import { skills } from "../constants";
+import { simonHero } from "../assets";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentTab = skills.find((skill) => {
+    return skill.id === currentIndex;
+  });
 
-export default About
+  return (
+    <section
+      id="about"
+      className={`${spacing.paddingY} text-center md:text-left`}
+    >
+      <Wrapper styles={`${layout.wrapper} space-y-8 md:space-y-24`}>
+        <header className="text-center">
+          <h2 className={typography.heading2}>About me</h2>
+        </header>
+        <div className="flex flex-col md:flex-row md:justify-between md:gap-10">
+          {/* text-container */}
+          <div className="flex-1">
+            <div class="border-b border-b-slate-400 dark:border-gray-700">
+              <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                <li class="mr-2">
+                  <button
+                    class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group active"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      class="mr-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    Education
+                  </button>
+                </li>
+                <li class="mr-2">
+                  <button
+                    class="inline-flex p-4 rounded-t-lg border-b-2 border-b-primary"
+                    aria-current="page"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      class="mr-2 w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                    </svg>
+                    Work
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div>{currentTab && <div>{currentTab.title}</div>}</div>
+          </div>
+
+          {/* image-container */}
+          <div className="space-y-4 w-full max-w-[550px] m-auto">
+            <h3 className={`${typography.heading3}`}>
+              I am <span className="text-primary">Simon Hernandez</span>
+            </h3>
+            <p>
+              ... a 23 year old Frontend Developer with a Bachelor's Degree in
+              Telecommunication Engineering.
+            </p>
+            <p>
+              I am proactive, organized and self-taught, capable of performing
+              tasks that require team work, problem solving, and strong
+              communication skills.
+            </p>
+            <p>Passionate about web technologies, telematics and baseball.</p>
+            
+<div id="default-carousel" class="relative" data-carousel="static">
+    {/* <!-- Carousel wrapper --> */}
+    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+         {/* <!-- Item 1 --> */}
+        <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item="">
+            <span class="absolute text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
+            <img src={simonHero} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        {/* <!-- Item 2 --> */}
+        <div class="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-full z-10" data-carousel-item="">
+            <img src={simonHero} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        {/* <!-- Item 3 --> */}
+        <div class="duration-700 ease-in-out absolute inset-0 transition-all transform -translate-x-full z-10" data-carousel-item="">
+            <img src={simonHero} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+    </div>
+    {/* <!-- Slider indicators --> */}
+    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+        <button type="button" class="w-3 h-3 rounded-full bg-white dark:bg-gray-800" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+    </div>
+    {/* <!-- Slider controls --> */}
+    <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev="">
+        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+            <span class="sr-only">Previous</span>
+        </span>
+    </button>
+    <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next="">
+        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+            <span class="sr-only">Next</span>
+        </span>
+    </button>
+</div>
+
+          </div>
+        </div>
+      </Wrapper>
+    </section>
+  );
+};
+
+export default About;
