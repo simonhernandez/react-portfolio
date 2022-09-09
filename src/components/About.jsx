d34cfc76043609b26ff41b05fa5226c9168cb343
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-09-09 19:47:33 -0400

    fix: tab index

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,8 +7,8 @@ import { simonHero } from "../assets";
 
 const About = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const currentTab = skills.find((skill) => {
-    return skill.id === currentIndex;
+  const currentTab = about.find((aboutItem) => {
+    return aboutItem.id === currentIndex;
   });
 
   return (
@@ -25,43 +25,6 @@ const About = () => {
           <div className="flex-1">
             <div class="border-b border-b-slate-400 dark:border-gray-700">
               <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
-                {/* <li class="mr-2">
-                  <button
-                    class="inline-flex p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group active"
-                  >
-                    <svg
-                      aria-hidden="true"
-                      class="mr-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                    Education
-                  </button>
-                </li>
-                <li class="mr-2">
-                  <button
-                    class="inline-flex p-4 rounded-t-lg border-b-2 border-b-primary text-primary bg-primary_very_light"
-                    aria-current="page"
-                  >
-                    <svg
-                      aria-hidden="true"
-                      class="mr-2 w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-                    </svg>
-                    Work
-                  </button>
-                </li> */}
                 {about.map((item, index) => {
                   return <li key={item.id} className={`${about.length !== index - 1 ? 'mr-2' : ''}`}>
                     <button
