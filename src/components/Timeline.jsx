5063e4e88430692de32195c95ddd67f0fda375a0
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-09-09 21:07:11 -0400

    add: timeline styles

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,10 +4,10 @@ import TimelineItem from './TimelineItem';
 
 const Timeline = ({aboutInfoArray}) => {
   return (
-    <ul>
+    <ul className='ml-16 my-8'>
         {aboutInfoArray.map((aboutInfoItem, index) =>{
             return (
-                <li key={aboutInfoItem.id} className={`${index !== aboutInfoArray.length - 1 ? 'mb-8' : ''}`}>
+                <li key={aboutInfoItem.id} className={`${index !== aboutInfoArray.length - 1 ? 'mb-8' : ''} timeline-item text-left`}>
                     <TimelineItem {...aboutInfoItem} />
                 </li>
             )
