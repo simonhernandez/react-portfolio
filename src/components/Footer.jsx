be17f7754e40f3fb6ba1e450ac989b878f9078be
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-09-11 20:00:59 -0400

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+
+import { Wrapper, Socials } from "../components";
+import { layout, typography, spacing } from "../styles/style";
+import { navLinks, socialLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className={`${spacing.paddingY} text-center md:text-left bg-dark text-white`}>
+      <Wrapper styles={`${layout.wrapper} flex flex-col space-y-10 md:flex-row md:justify-between md:items-center md:space-y-0`}>
+        <div className="brand">
+          <h2 className={`${typography.heading2} text-primary`}>
+            <a href="/">Simon Hernandez</a>
+          </h2>
+          <small className="copyright">
+            &copy; 2022 copyright all rights reserved
+          </small>
+        </div>
 
-export default Footer
+        <nav>
+          <ul className="md:flex md:flex-wrap md:justify-between md:max-w-[180px]">
+            {navLinks.map((navlink, index) => {
+              return (
+                <li
+                  key={navlink.id}
+                  className={`py-1 px-2 rounded text-center text-sm leading-none hover:text-primary md:basis-[89px] md:text-left ${
+                    index !== navLinks.length - 1 ? "mb-4" : ""
+                  }`}
+                >
+                  <a href={`#${navlink.id}`}>{navlink.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+
+        <div className="flex justify-center items-center">
+          <Socials socials={socialLinks} styles={`flex gap-4`} />
+        </div>
+      </Wrapper>
+    </footer>
+  );
+};
+
+export default Footer;
