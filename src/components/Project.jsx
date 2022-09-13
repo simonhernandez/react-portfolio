0ae9d23e9adea4b36a5b5d01a69258b487ecb986
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-09-12 22:28:33 -0400

    add: project styles

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,20 +3,19 @@ import { typography } from "../styles/style";
 
 const Project = ({ id, img, title, description, technologies, link }) => {
   return (
-    <div className="min-w-[300px] max-w-[350px] mx-auto shadow-project rounded-md overflow-hidden text-left md:col-span-6 lg:col-span-4">
+    <div className="project min-w-[300px] max-w-[350px] mx-auto shadow-lg rounded-2xl overflow-hidden text-left md:col-span-6 lg:col-span-4">
       <div className="overflow-hidden">
         <img
           src={img}
           alt={`A Preview Image of a the ${title} Project`}
-          className=" scale-110 transition-all"
         />
       </div>
 
-      <div className="p-2 space-y-2">
+      <div className="p-2 space-y-2 md:p-4">
         <ul className="flex justify-start items-center">
           {technologies.map((technology, index) => {
             return (
-                <li key={index} className={`bg-primary_light py-[2.777px] px-[8.333px] rounded-sm ${technologies.length - 1 !== index ? 'mr-1' : ''}`}>{technology}</li>
+                <li key={index} className={`bg-primary_light py-[2.777px] px-[8.333px] rounded-sm text-xs font-bold text-primary ${technologies.length - 1 !== index ? 'mr-1' : ''}`}>{technology}</li>
             )
           })}
         </ul>
@@ -30,19 +29,19 @@ const Project = ({ id, img, title, description, technologies, link }) => {
         <div className="flex justify-between items-center">
           <a
             href={link.repo}
-            className="btn btn--ghost btn--small w-[48%] flex justify-center items-center"
+            className="btn btn--ghost btn--small w-[48%] flex justify-center items-center border border-primary rounded-sm py-2"
             target="_blank"
           >
-            <span className="text_xs mr-2">View Code</span>
+            <span className="text-xs mr-2 uppercase text-primary font-bold">View Code</span>
             <i className="bx bx-link-external"></i>
           </a>
 
           <a
             href={link.site}
-            className="btn btn--ghost btn--small"
+            className="btn btn--ghost btn--small w-[48%] flex justify-center items-center border border-primary rounded-sm py-2"
             target="_blank"
           >
-            <span className="text-xs">View Site</span>
+            <span className="text-xs mr-2 uppercase text-primary font-bold">View Site</span>
             <i className="bx bx-link-external"></i>
           </a>
         </div>
