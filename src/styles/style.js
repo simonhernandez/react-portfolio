4f3451dead37dfb4d48e61c86d046c8bb1475469
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-09-21 14:40:37 -0400

    fix: header height

--- src/styles/style.js
+++ src/styles/style.js
@@ -5,7 +5,7 @@ export const spacing = {
     marginX: "sm:mx-16 mx-6",
     marginY: "sm:my-16 my-6",
 
-    headerHeight: "h-16 sm:h-20",
+    headerHeight: "h-16",
 }
 
 export const typography = {
